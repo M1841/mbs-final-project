cfb52d70e0e7410e1cb192de709f57c4f7da78c9
M1841 <mihaimuresan592003@gmail.com>
2024-05-27 18:48:25 +0300

    Small Tweaks

--- main.ino
+++ main.ino
@@ -1,4 +1,16 @@
 #include <NewPing.h>
+#include <SoftwareSerial.h>
+
+#define RECEIVER_PIN 8
+#define TRANSMITTER_PIN 9
+
+#define TRIG_PIN A4
+#define ECHO_PIN A5
+#define MAX_DISTANCE 200
+
+SoftwareSerial bluetooth(RECEIVER_PIN, TRANSMITTER_PIN);
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+char lastCommand = 'S';
 
 class Movement
 {
@@ -37,23 +49,8 @@ public:
       break;
     }
   }
-  static void stop()
-  {
-    digitalWrite(RIGHT_FRONT, 0);
-    digitalWrite(LEFT_FRONT, 0);
-    digitalWrite(RIGHT_BACK, 0);
-    digitalWrite(LEFT_BACK, 0);
-  }
 
-private:
-  static const uint8_t RIGHT_BACK = 2;
-  static const uint8_t RIGHT_FRONT = 3;
-  static const uint8_t LEFT_FRONT = 4;
-  static const uint8_t LEFT_BACK = 5;
-
-  static const uint8_t MOTOR_A = 1;
-  static const uint8_t MOTOR_B = 6;
-
+public:
   static void front()
   {
     digitalWrite(RIGHT_FRONT, 1);
@@ -82,78 +79,82 @@ private:
     digitalWrite(RIGHT_BACK, 1);
     digitalWrite(LEFT_BACK, 0);
   }
+  static void stop()
+  {
+    digitalWrite(RIGHT_FRONT, 0);
+    digitalWrite(LEFT_FRONT, 0);
+    digitalWrite(RIGHT_BACK, 0);
+    digitalWrite(LEFT_BACK, 0);
+  }
+
+  static const uint8_t RIGHT_BACK = 2;
+  static const uint8_t RIGHT_FRONT = 3;
+  static const uint8_t LEFT_FRONT = 4;
+  static const uint8_t LEFT_BACK = 5;
+
+  static const uint8_t MOTOR_A = 1;
+  static const uint8_t MOTOR_B = 6;
 };
-
-class Bluetooth
-{
-public:
-  static void setup()
-  {
-    Serial.begin(9600);
-  }
-  static char read()
-  {
-    if (Serial.available() > 0)
-    {
-      command = Serial.read();
-    }
-    return command;
-  }
-
-private:
-  static char command;
-};
-
-char Bluetooth::command;
-
-class DistanceSensor
-{
-public:
-  static void setup()
-  {
-    sonar = NewPing(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
-    delay(2000);
-    distance = read();
-    delay(100);
-    distance = read();
-    delay(100);
-    distance = read();
-    delay(100);
-    distance = read();
-    delay(100);
-  }
-  static int read()
-  {
-    delay(70);
-    distance = sonar.ping_cm();
-    return distance;
-  }
-
-private:
-  static const uint8_t TRIG_PIN = A4;
-  static const uint8_t ECHO_PIN = A5;
-  static const uint8_t MAX_DISTANCE = 200;
-  static uint8_t distance;
-  static NewPing sonar;
-};
-uint8_t DistanceSensor::distance;
-NewPing DistanceSensor::sonar(DistanceSensor::TRIG_PIN, DistanceSensor::ECHO_PIN, DistanceSensor::MAX_DISTANCE);
 
 void setup()
 {
   Movement::setup();
-  Bluetooth::setup();
-  DistanceSensor::setup();
+  bluetooth.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  if (DistanceSensor::read() > 20)
+  delay(5);
+  Serial.println(lastCommand);
+  Movement::move(lastCommand);
+
+  char command;
+
+  if (bluetooth.available() > 0)
   {
-    Movement::move(Bluetooth::read());
+    command = bluetooth.read();
+    if (command != 'F' && command != 'B' && command != 'L' && command != 'R' && command != 'S')
+    {
+      command = lastCommand;
+    }
   }
   else
   {
-    Movement::stop();
+    command = lastCommand;
   }
+
+  if (command == 'F')
+  {
+    uint8_t distance = sonar.ping_cm();
+    distance = distance ? distance : 255;
+    if (distance < 20)
+    {
+      command = 'S';
+    }
+  }
+
+  if (lastCommand != command)
+  {
+    switch (command)
+    {
+    case 'F':
+      bluetooth.println("Going Forward");
+      break;
+    case 'B':
+      bluetooth.println("Going Backwards");
+      break;
+    case 'L':
+      bluetooth.println("Turning Left");
+      break;
+    case 'R':
+      bluetooth.println("Turning Right");
+      break;
+    case 'S':
+      bluetooth.println("Stopping");
+      break;
+    }
+  }
+
+  lastCommand = command;
 }
